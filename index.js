526b133a6384bd0a4d373d790fddc8050d982dd5
William Falcone <wjf0003@gmail.com>
2020-04-01 13:08:40 -0400

    create date function

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 
 let DAYS = { 0: 'Sunday', 1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday'}
-let MONTH = {}
+let MONTH = { 0: 'January', 
+              1: 'February', 
+              2: 'March', 
+              3: 'April', 
+              4: 'May', 
+              5: 'June', 
+              6: 'July', 
+              7: 'August', 
+              8: 'September', 
+              9: 'October',
+              10: 'November',
+              11: 'December' }
+
+
 
 function today(){
   let d = new Date()
   let day = d.getDay()
   let mon = d.getMonth()
-  console.log(DAYS[day])
-  console.log(mon)
+  let year = d.getFullYear()
+
+  let tag = document.getElementById('date')
+  tag.innerHTML = `Today is ${DAYS[day]},&nbsp  ${MONTH[mon]} ${year} `
 }
 
 today()
